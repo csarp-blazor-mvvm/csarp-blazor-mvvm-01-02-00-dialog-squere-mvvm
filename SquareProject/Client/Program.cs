f8ad16497777d219f6ff75c427e1ca5a6b2384aa
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-25 17:58:51 +0200

    Projekt működik

---
+++ SquareProject/Client/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SquareProject.Client;
+using SquareProject.Client.ViewModels;
+using SquareProject.ViewModels;
+using ViewModels.BaseClass;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<ISquareViewModel, SquareViewModel>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
